d35a7180e78fe7c054596e17b4a8d79e282218ee
Kilo Developer <developer@example.com>
2026-04-26 14:06:11 +0000

    Add waiter commission feature: kitchen 2%, bar tiered 1%-11% based on sales, currency Ksh

---
+++ .kilocode/rules/memory-bank/context.md
@@ -0,0 +1,20 @@
+# Memory Bank Context
+
+## Recently Completed
+- [x] Added waiter commission feature for sales made
+- [x] Created CommissionUtils utility class for calculating commissions
+- [x] Kitchen commission: Fixed 2% of food sales
+- [x] Bar commission: Tiered structure (1%-11% based on sales amount)
+- [x] Currency formatted as Ksh (Kenyan Shilling)
+- [x] Added login.image.path property to kalcconfig.properties
+- [x] Modified LoginDialog to remove username field and use 8-digit PIN only
+- [x] Added restaurant image display to login page loaded from kalcconfig.properties
+- [x] Created PropertyUtil utility class to read properties from kalcconfig.properties
+- [x] Updated JAlertPane.loginDialog() to return empty string for username (since not used)
+- [x] Created LoginSettingsDialog for admin to configure restaurant image path
+- [x] Fixed AppConfig.getProperty() to return actual property values
+
+## Current State
+- Waiting commission feature implemented and ready for testing
+- Login page changes completed and committed
+- Repository is clean with all changes committed locally
---
+++ src-pos/ke/kalc/pos/util/CommissionUtils.java
@@ -0,0 +1,82 @@
+/*
+ **    KALC POS  - Professional Point of Sale
+ **
+ **    This file is part of KALC POS Version KALC V1.5.4
+ **
+ **    Copyright (c) 2015-2023 KALC & previous Openbravo POS related works   
+ **
+ **    https://www.KALC.co.uk
+ **
+ **
+ */
+
+package ke.kalc.pos.util;
+
+/**
+ * Utility class for calculating waiter commissions based on sales
+ */
+public class CommissionUtils {
+
+    private CommissionUtils() {
+        // Utility class - prevent instantiation
+    }
+
+    /**
+     * Calculate kitchen commission (fixed 2% of food sales)
+     * @param foodSales Amount of food sales
+     * @return Kitchen commission amount
+     */
+    public static double calculateKitchenCommission(double foodSales) {
+        return foodSales * 0.02;
+    }
+
+    /**
+     * Calculate bar commission based on tiered structure
+     * @param barSales Amount of bar sales
+     * @return Bar commission amount
+     */
+    public static double calculateBarCommission(double barSales) {
+        if (barSales <= 100000) {
+            return barSales * 0.01;
+        } else if (barSales <= 200000) {
+            return barSales * 0.02;
+        } else if (barSales <= 300000) {
+            return barSales * 0.03;
+        } else if (barSales <= 400000) {
+            return barSales * 0.04;
+        } else if (barSales <= 500000) {
+            return barSales * 0.05;
+        } else if (barSales <= 600000) {
+            return barSales * 0.06;
+        } else if (barSales <= 700000) {
+            return barSales * 0.07;
+        } else if (barSales <= 800000) {
+            return barSales * 0.08;
+        } else if (barSales <= 900000) {
+            return barSales * 0.09;
+        } else if (barSales <= 1000000) {
+            return barSales * 0.10;
+        } else {
+            return barSales * 0.11;
+        }
+    }
+
+    /**
+     * Calculate total waiter commission (kitchen + bar)
+     * @param foodSales Amount of food sales
+     * @param barSales Amount of bar sales
+     * @return Total commission amount
+     */
+    public static double calculateTotalCommission(double foodSales, double barSales) {
+        return calculateKitchenCommission(foodSales) + calculateBarCommission(barSales);
+    }
+
+    /**
+     * Format commission amount as currency string (Ksh)
+     * @param amount Commission amount
+     * @return Formatted currency string
+     */
+    public static String formatCommission(double amount) {
+        return String.format("Ksh %.2f", amount);
+    }
+}
