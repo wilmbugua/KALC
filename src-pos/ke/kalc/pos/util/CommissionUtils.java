1f6557c1682c0d6bdd3f1e71da0b2f9bc9199da2
KALC Developer <dev@kalc.co.ke>
2026-05-05 13:32:53 +0000

    Add BigDecimal methods to CommissionUtils for precise monetary calculations

--- src-pos/ke/kalc/pos/util/CommissionUtils.java
+++ src-pos/ke/kalc/pos/util/CommissionUtils.java
@@ -12,6 +12,9 @@
 
 package ke.kalc.pos.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Utility class for calculating waiter commissions based on sales.
  * <p>
@@ -20,6 +23,11 @@ package ke.kalc.pos.util;
  *   <li>Kitchen commission: fixed 2% of food sales</li>
  *   <li>Bar commission: tiered structure based on bar sales amount</li>
  * </ul>
+ * <p>
+ * Precision Handling:
+ * - All monetary values are represented using BigDecimal.
+ * - Methods are designed to maintain a scale of 2 decimal places for currency calculations.
+ * - Rounding is done using RoundingMode.HALF_UP to follow standard monetary rounding rules.
  */
 public class CommissionUtils {
 
@@ -95,5 +103,82 @@ public class CommissionUtils {
      */
     public static String formatCommission(double amount) {
         return String.format("Ksh %.2f", amount);
+    }
+
+    /**
+     * Calculates the commission based on a given amount using BigDecimal for precision.
+     * 
+     * @param amount The amount for which the commission is calculated. Must not be null.
+     * @return The calculated commission as BigDecimal, rounded to 2 decimal places.
+     */
+    public static BigDecimal calculateCommission(BigDecimal amount) {
+        BigDecimal commissionRate = new BigDecimal("0.10");
+        BigDecimal commission = amount.multiply(commissionRate);
+        return commission.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Calculate kitchen commission (fixed 2% of food sales) using BigDecimal
+     * @param foodSales Amount of food sales
+     * @return Kitchen commission amount
+     */
+    public static BigDecimal calculateKitchenCommission(BigDecimal foodSales) {
+        BigDecimal rate = new BigDecimal("0.02");
+        return foodSales.multiply(rate).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Calculate bar commission based on tiered structure using BigDecimal
+     * @param barSales Amount of bar sales
+     * @return Bar commission amount
+     */
+    public static BigDecimal calculateBarCommission(BigDecimal barSales) {
+        BigDecimal[] tiers = {
+            new BigDecimal("100000"),
+            new BigDecimal("200000"),
+            new BigDecimal("300000"),
+            new BigDecimal("400000"),
+            new BigDecimal("500000"),
+            new BigDecimal("600000"),
+            new BigDecimal("700000"),
+            new BigDecimal("800000"),
+            new BigDecimal("900000"),
+            new BigDecimal("1000000")
+        };
+        BigDecimal rate;
+        if (barSales.compareTo(tiers[0]) <= 0) {
+            rate = new BigDecimal("0.01");
+        } else if (barSales.compareTo(tiers[1]) <= 0) {
+            rate = new BigDecimal("0.02");
+        } else if (barSales.compareTo(tiers[2]) <= 0) {
+            rate = new BigDecimal("0.03");
+        } else if (barSales.compareTo(tiers[3]) <= 0) {
+            rate = new BigDecimal("0.04");
+        } else if (barSales.compareTo(tiers[4]) <= 0) {
+            rate = new BigDecimal("0.05");
+        } else if (barSales.compareTo(tiers[5]) <= 0) {
+            rate = new BigDecimal("0.06");
+        } else if (barSales.compareTo(tiers[6]) <= 0) {
+            rate = new BigDecimal("0.07");
+        } else if (barSales.compareTo(tiers[7]) <= 0) {
+            rate = new BigDecimal("0.08");
+        } else if (barSales.compareTo(tiers[8]) <= 0) {
+            rate = new BigDecimal("0.09");
+        } else if (barSales.compareTo(tiers[9]) <= 0) {
+            rate = new BigDecimal("0.10");
+        } else {
+            rate = new BigDecimal("0.11");
+        }
+        return barSales.multiply(rate).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Calculate total waiter commission (kitchen + bar) using BigDecimal
+     * @param foodSales Amount of food sales
+     * @param barSales Amount of bar sales
+     * @return Total commission amount
+     */
+    public static BigDecimal calculateTotalCommission(BigDecimal foodSales, BigDecimal barSales) {
+        return calculateKitchenCommission(foodSales).add(calculateBarCommission(barSales));
     }
 }
